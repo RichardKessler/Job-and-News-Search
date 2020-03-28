cdd5e6e943443d12b6016f4c662dc1e39675b284
JidMenosta <jidmenosta@gmail.com>
2020-03-28 13:58:37 -0400

    added news.js to connect to the news api

---
+++ news.js
@@ -0,0 +1,21 @@
+// var req = new Request(url);
+
+$("#search").click(function(event) {
+    event.preventDefault();
+
+    var searchTerm = $("#searchKey").val();
+    var url1 = 'http://newsapi.org/v2/everything?' + 'qInTitle=' + searchTerm + '&' + 'sortBy=relevancy&language=en&pageSize=50&apiKey=645a8c4dfc5044a0845c6033b3728a59';
+
+    // $.ajax({
+    //         url: url1,
+    //         method: 'GET'
+    //     })
+    //     .then(function(response) {
+    //         console.log("response", response)
+
+    //     })
+
+    $.get(url1, function(response) {
+        console.log(response);
+    })
+});
